b9e996e550f0b9a1a21f6fbd9f17d5755f759799
ok2385 <hhj4669@gmail.com>
2024-07-11 11:35:41 +0900

    add image to sidebar, add scss folder,css files

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,20 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import icon1 from "./assets/power.png";
+import icon2 from "./assets/processing.png";
+import icon3 from "./assets/reservationstatus.png";
+import icon4 from "./assets/sales-inquiry.png";
+import icon5 from "./assets/store-setting.png";
 import "./Sidebar.css"
 
 const Sidebar = () => {
   return(
       <div className={"sidebar"}>
-          <Link to={"/processing"}>처리중</Link>
-          <Link to={"/reservation-status"}>예약현황</Link>
-          <Link to={"/sales-inquiry"}>매출조회</Link>
-          <Link to={"/store-setting"}>가게설정</Link>
-          <Link to={"/close-store"}>영업종료</Link>
+          <Link to={"/processing"}><img src={icon2} alt={"처리중"}/>처리중</Link>
+          <Link to={"/reservation-status"}><img src={icon3} alt={"예약현황"}/>예약현황</Link>
+          <Link to={"/sales-inquiry"}><img src={icon4} alt={"매출조회"}/>매출조회</Link>
+          <Link to={"/store-setting"}><img src={icon5} alt={"가게설정"}/>가게설정</Link>
+          <Link to={"/close-store"}><img src={icon1} alt={"영업종료"}/>영업종료</Link>
       </div>
   )
 }
