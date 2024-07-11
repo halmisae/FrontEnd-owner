183aa697ed35c92d2247e7e2039e9a7254be3526
ok2385 <hhj4669@gmail.com>
2024-07-11 09:30:44 +0900

    add Page, remake App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+
+import Home from "./pages/Home";
+import BookManage from "./pages/BookManage";
+import SellManage from "./pages/SellManage";
+import ShopManage from "./pages/SellManage";
+import Processing from "./pages/Processing";
+import Exit from "./pages/Exit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Home/>}/>
+          <Route path={"/"} element={<BookManage/>}/>
+          <Route path={"/"} element={<SellManage/>}/>
+          <Route path={"/"} element={<ShopManage/>}/>
+          <Route path={"/"} element={<Processing/>}/>
+          <Route path={"/"} element={<Exit/>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
