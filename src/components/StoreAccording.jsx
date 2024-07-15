4d13c2b4d31bf0e2c3e438747bad400e508e7d19
ok2385 <hhj4669@gmail.com>
2024-07-15 10:22:59 +0900

    add back-space image to according title

--- src/components/StoreAccording.jsx
+++ src/components/StoreAccording.jsx
@@ -1,8 +1,11 @@
 import React,{useState}from "react";
-import "../scss/StoreAccording.css"
+import {useNavigate} from "react-router-dom";
+import backIcon from "../assets/back-space.png";
+import "../scss/StoreAccording.css";
 
 const StoreAccording = ({title,icon,children,isCollapsible, alwaysVisible}) =>{
     const [isOpen, setIsOpen] = useState(alwaysVisible);
+    const navigate = useNavigate();
 
     const toggleAction  =()=>{
         if (isCollapsible){
@@ -10,11 +13,23 @@ const StoreAccording = ({title,icon,children,isCollapsible, alwaysVisible}) =>{
         }
     };
 
+    const handleGoBack =()=>{
+        navigate(-1);
+    };
+
     return(
         <div className={"according"}>
             <button className={"according-title"} onClick={toggleAction}>
                 <span className={"icon"}>{icon}</span>
                 {title}
+                {alwaysVisible && (
+                    <img
+                        src={backIcon}
+                        alt={"뒤로 가기"}
+                        className={"back-icon"}
+                        onClick={handleGoBack}
+                    />
+                )}
             </button>
             {(isOpen || alwaysVisible)&& (<div className={"according-content"}>{children}</div>)}
         </div>
